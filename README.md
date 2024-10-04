5b7e189ebef49f1903da11fb9fdd79f6d5ec8da4
Sital Adhikari <sitaladhikari002@gmail.com>
2024-10-04 11:09:41 +0545

    initialize project with nextjs

---
+++ README.md
@@ -0,0 +1,10 @@
+# Ecommerce Application
+
+This is a simple Ecommerce Application built using Next.js(App Router) This application is built for educational purposes only.
+
+## How to run it locally
+
+1. Clone the repository
+2. Run `pnpm install` to install the dependencies
+3. Copy the `.env.example` file to `.env.local` and fill in the required environment variables
+4. Run `pnpm dev` to start the development server
---
+++ src/app/page.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+import { LatestPost } from "~/app/_components/post";
+import { env } from "~/env";
+import { getServerAuthSession } from "~/server/auth";
+import { api, HydrateClient } from "~/trpc/server";
+
+export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+  const session = await getServerAuthSession();
+  console.log(session, env.GITHUB_CLIENT_ID);
+
+  void api.post.getLatest.prefetch();
+
+  return (
+    <HydrateClient>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+          <div className="flex flex-col items-center gap-2">
+            <div className="flex flex-col items-center justify-center gap-4">
+              <p className="text-center text-2xl text-white">
+                {session && <span>Logged in as {session.user?.name}</span>}
+              </p>
+              <Link
+                href={session ? "/api/auth/signout" : "/api/auth/signin"}
+                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+              >
+                {session ? "Sign out" : "Sign in"}
+              </Link>
+            </div>
+          </div>
+        </div>
+      </main>
+    </HydrateClient>
+  );
+}
